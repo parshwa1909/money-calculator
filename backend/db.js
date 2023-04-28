2a03c8b5ec8ae4b75e7fae70ca075a095c9310a7
parshwa <parshwagandhi63@gmail.com>
2023-04-28 14:07:38 +0530

    my latest update 2

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
+require("dotenv/config");
 
 const connectToMongo = () => {
-    const mongoURL =
-        "mongodb+srv://parshwa2001:<password>@parshwa19.jqxc2ra.mongodb.net/?retryWrites=true&w=majority";
+    const dbUsername = process.env.DB_USERNAME;
+    const dbPassword = process.env.DB_PASSWORD;
+
+    const mongoURL = `mongodb+srv://${dbUsername}:${dbPassword}@parshwa19.jqxc2ra.mongodb.net/?retryWrites=true&w=majority`;
+
     mongoose.connect(mongoURL, () => {
         console.log("connected to mongo");
     });
